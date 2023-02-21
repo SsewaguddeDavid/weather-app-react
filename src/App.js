6f2b5a65d9bc8d807ec40488bc10f5c21cfbb72e
Ssewagudde David <dsewagude10@gmail.com>
2023-02-21 23:46:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6f90e27aefc0c9f17d253232aa9f727b`;
+
+  const searchLocation = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange = {e => setLocation(e.target.value)}
+        onKeyPress = {searchLocation}
+        placeholder = 'Enter Location'
+        type="text" 
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            <h1>60F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">20 MPH</p>
+            <p>Wind speed</p>
+          </div>
+        </div>
+      </div>
+    
     </div>
   );
 }
